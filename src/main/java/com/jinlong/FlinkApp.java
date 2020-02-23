70d937cc37a9a929571a360d0e7a6f2781c36ab6
jinlong <jinlong@qianxin.com>
2020-02-23 12:28:40 +0800

    flink依赖

--- src/main/java/com/jinlong/FlinkApp.java
+++ src/main/java/com/jinlong/FlinkApp.java
@@ -7,14 +7,16 @@ import java.util.List;
 
 /**
  * @author jinlong
+ * String yamlPath = "src/main/resources/application.yml";
  */
 @Slf4j
 public class FlinkApp {
     public static void main(String[] args) {
         log.info("app启动");
         YamlConf yamlConf = new YamlConf();
-        String yamlPath = "classpath:application.yml";
+        String yamlPath = "src/main/resources/application.yml";
         List list = yamlConf.getYamlConf(yamlPath);
+        log.info("{}",list.size());
         list.forEach(i->log.info("{}",i));
     }
 }
